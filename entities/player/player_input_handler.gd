700f36e18dedda72814080e602c7b4c92418a0fb
phuocn0302 <phuocnguyen230302@gmail.com>
2025-07-10 22:31:59 +0700

    feat: add input map for movement

--- entities/player/player_input_handler.gd
+++ entities/player/player_input_handler.gd
@@ -12,9 +12,10 @@ func setup(_player: Player, _state_machine: StateMachine):
 
 
 func _input(event: InputEvent) -> void:
-	player.input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	player.input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
-	if event.is_action_pressed("ui_accept") and player.dash_state.dash_cooldown_timer.is_stopped():
+	if event.is_action_pressed("dash") and player.dash_state.dash_cooldown_timer.is_stopped():
 		state_machine.change_state(states["Dash"])
+	
 	
 	state_machine.process_input(event)
