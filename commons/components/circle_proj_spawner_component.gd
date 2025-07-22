38eb0caf12f49889cc95285ba32f4108c4f12f12
phuocn0302 <phuocnguyen230302@gmail.com>
2025-07-22 13:27:17 +0700

    feat: add circular projectile spawner

---
+++ commons/components/circle_proj_spawner_component.gd
@@ -0,0 +1,22 @@
+class_name CircleProjectileSpawner
+extends ProjectileSpawner
+
+@export var number_of_projectile: int = 10
+@export var rotate_pattern: bool = false
+
+@export_category("Rotation")
+# In degree
+@export_range(-360, 360) var rotate_speed: float = 20
+
+func spawn() -> void:
+	var angle := TAU / number_of_projectile
+	
+	for i in range(number_of_projectile):
+		var _projectile := object_pool.get_instance() as Projectile
+		_projectile.direction = self.projectile_direction.rotated(angle * i) 
+		_projectile.global_position = self.global_position
+	
+	if rotate_pattern:
+		projectile_direction = projectile_direction.rotated(
+			rad_to_deg(rotate_speed) * get_physics_process_delta_time()
+		)
