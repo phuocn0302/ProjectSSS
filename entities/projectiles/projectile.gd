e3fe94e7612143eb1343389f02f38717c9c6e611
phuocn0302 <phuocnguyen230302@gmail.com>
2025-07-23 15:57:56 +0700

    feat (projectile): add on hit/hide sfx option

--- entities/projectiles/projectile.gd
+++ entities/projectiles/projectile.gd
@@ -5,6 +5,9 @@ extends PoolableEntity
 @export var max_speed: float = 300
 @export var lifetime: float = 1
 
+@export var on_hit_sfx: PackedScene
+@export var on_hide_sfx: PackedScene
+
 var lifetime_timer: SceneTreeTimer
 
 func _ready() -> void:
@@ -16,7 +19,18 @@ func active() -> void:
 	start_lifetime_timer()
 
 
+func deactive() -> void:
+	super.deactive()
+	
+	if on_hide_sfx:
+		_spawn_sfx(on_hide_sfx)
+
+
 func _process(delta: float) -> void:
+	move(delta)
+
+
+func move(delta: float) -> void:
 	self.global_rotation = direction.angle()
 	self.global_position += direction.normalized() * max_speed * delta
 
@@ -31,3 +45,12 @@ func start_lifetime_timer():
 
 func _on_hitbox_component_hit() -> void:
 	self.deactive()
+	
+	if on_hit_sfx: 
+		_spawn_sfx(on_hit_sfx)
+
+
+func _spawn_sfx(sfx: PackedScene) -> void:
+	var _sfx = sfx.instantiate()
+	_sfx.global_position = self.global_position
+	get_tree().current_scene.add_child(_sfx)
