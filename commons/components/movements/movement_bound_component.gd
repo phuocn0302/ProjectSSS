a4d2eca2642484b903ba938110acb0886c35bde2
phuocn0302 <phuocnguyen230302@gmail.com>
2025-07-28 14:34:10 +0700

    feat: add movement_bound_component limit area of movement

---
+++ commons/components/movements/movement_bound_component.gd
@@ -0,0 +1,10 @@
+class_name MovementBoundsComponent
+extends Component
+
+## Origin and (Width, Height)
+@export var bounds: Array[Vector2] = [Vector2.ZERO, Vector2(180, 320)]
+
+
+func _physics_process(delta: float) -> void:
+	entity.global_position.x = clamp(entity.global_position.x, bounds[0].x, bounds[1].x)
+	entity.global_position.y = clamp(entity.global_position.y, bounds[0].y, bounds[1].y)
