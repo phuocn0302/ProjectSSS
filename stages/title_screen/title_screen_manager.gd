d70a97700b7cdf3433ac300fb69deda7590a07f1
phuocn0302 <phuocnguyen230302@gmail.com>
2025-08-06 19:37:22 +0700

    feat: add title screen ui

---
+++ stages/title_screen/title_screen_manager.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+@export var button_move_amount: int = 10
+@export var hover_anim_time: float = 0.2
+
+var _buttons_default_location: Dictionary[TextureButton, Vector2]
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var start_button: TextureButton = %StartButton
+@onready var options_button: TextureButton = %OptionsButton
+@onready var quit_button: TextureButton = %QuitButton
+@onready var buttons: Control = $Buttons
+
+func _ready() -> void:
+	pass
+
+
+func _setup_button() -> void:
+	for b in buttons.get_children():
+		if b is TextureButton:
+			b.disabled = false
+			_buttons_default_location[b as TextureButton] = b.position
+			
+			b.mouse_entered.connect(Callable(self, "_hover_button").bind(b))
+			b.mouse_exited.connect(Callable(self, "_unhover_button").bind(b))
+
+
+func _hover_button(button: TextureButton) -> void:
+	var tween := create_tween()
+	
+	tween.tween_property(
+		button,
+		"position", 
+		button.position + Vector2.LEFT * button_move_amount,
+		hover_anim_time
+		)
+
+
+func _unhover_button(button: TextureButton) -> void:
+	var tween := create_tween()
+	
+	tween.tween_property(
+		button,
+		"position", 
+		_buttons_default_location[button],
+		hover_anim_time
+		)
+
+
+func _on_start_button_pressed() -> void:
+	pass
+
+
+func _on_options_button_pressed() -> void:
+	pass
+
+
+func _on_quit_button_pressed() -> void:
+	pass
