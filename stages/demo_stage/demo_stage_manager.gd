19f335a8bcaed7a66f15380ca6489d307c0f407d
phuocn0302 <phuocnguyen230302@gmail.com>
2025-08-06 22:47:35 +0700

    feat: add stage_retry

--- stages/demo_stage/demo_stage_manager.gd
+++ stages/demo_stage/demo_stage_manager.gd
@@ -1,7 +1,11 @@
 class_name DemoStageManager
 extends Node2D
 
+const GODOT_BOSS = preload("res://entities/bosses/godot/godot_boss.tscn")
 const GODOT_ENEMY = preload("res://entities/enemies/godot_enemy/godot_enemy.tscn")
+
+const STAGE_RETRY = preload("res://utils/scenes/ui/stage_retry.tscn")
+const DEMO_STAGE = preload("res://stages/demo_stage/demo_stage.tscn")
 
 @export var boss: PackedScene
 @export var enemy_spawn_pos: Array[Vector2]
@@ -9,20 +13,23 @@ const GODOT_ENEMY = preload("res://entities/enemies/godot_enemy/godot_enemy.tscn
 @onready var input_hint: Label = $CanvasLayer/InputHint
 @onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
 @onready var boss_health_bar: ProgressBar = $CanvasLayer/BossHealthBar
+@onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 @onready var player: Player = $Player
 
 var _score: int = 0
 
 func _ready() -> void:
-	assert(boss)
 	assert(not enemy_spawn_pos.is_empty())
+	
+	if not boss:
+		boss = GODOT_BOSS
 	
 	enemy_spawn_timer.autostart = false
 	enemy_spawn_timer.timeout.connect(_spawn_enemy)
 	
 	boss_health_bar.visible = false
-	player.die.connect(_reload)
+	player.die.connect(_on_player_die)
 
 
 func _process(_delta: float) -> void:
@@ -67,7 +74,7 @@ func _update_score(amount: int) -> void:
 
 func _on_boss_defeated() -> void:
 	boss_health_bar.visible = false
-	_reload(3)
+	
 
 
 func _setup_health_bar(max_health: float) -> void:
@@ -105,10 +112,29 @@ func disable_input_hint() -> void:
 	tween.tween_property(input_hint, "modulate:a", 0, 1)
 
 
-func _reload(delay: float = 1) -> void:
-	await get_tree().create_timer(delay).timeout
+func _on_player_die() -> void:
+	await get_tree().create_timer(1).timeout
 	
-	var fade = $CanvasLayer/ColorRect
-	var tween = create_tween()
-	tween.tween_property(fade, "modulate:a", 1, 1)
-	tween.finished.connect(func(): get_tree().reload_current_scene())
+	var retry = STAGE_RETRY.instantiate()
+	
+	retry.yes_pressed.connect(_on_retry)
+	retry.no_pressed.connect(_return_to_title)
+	
+	canvas_layer.add_child(retry)
+
+
+func _on_retry() -> void:
+	var tree = get_tree()
+	var new_stage = DEMO_STAGE.instantiate() as DemoStageManager
+	new_stage.boss = boss
+
+	if tree.current_scene:
+		tree.current_scene.queue_free()
+
+	tree.root.add_child(new_stage)
+	tree.current_scene = new_stage
+
+
+func _return_to_title() -> void:
+	# Dont know why change scene to packed isnt working here
+	get_tree().change_scene_to_file("res://stages/title_screen/title_screen.tscn")
