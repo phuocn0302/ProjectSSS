1bd5253af3b86219cdb91b9077343d0c547d5158
phuocn0302 <phuocnguyen230302@gmail.com>
2025-08-21 20:42:34 +0700

    feat: particles convert to kotlin

--- src/particles/oneshot_particles.gd
+++
@@ -1,9 +0,0 @@
-class_name OneshotParticles
-extends GPUParticles2D
-
-
-func _ready() -> void:
-	self.emitting = false
-	self.one_shot = true
-	self.emitting = true
-	self.finished.connect(queue_free)
