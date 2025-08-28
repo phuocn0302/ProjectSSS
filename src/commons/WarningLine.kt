e0d1346ad69928bf7e047bffbdb0ae3bcfa3eed9
phuocn0302 <phuocnguyen230302@gmail.com>
2025-08-28 21:04:32 +0700

    fix (WarningLine): not updating endpoint and not repeat texture

--- src/commons/WarningLine.kt
+++ src/commons/WarningLine.kt
@@ -1,5 +1,6 @@
 package commons
 
+import godot.annotation.Export
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.annotation.RegisterProperty
@@ -13,12 +14,19 @@ import godot.global.GD
 @RegisterClass
 class MoveWarningLine : Line2D() {
 
+    @Export
     @RegisterProperty
     var lineTexture: Texture2D? = GD.load("res://assets/texture/sfx/line/med_move_warning_line.png")
+
+    @Export
     @RegisterProperty
     var showTime: Double = 0.5
+
+    @Export
     @RegisterProperty
     var direction: Vector2 = Vector2.DOWN
+
+    @Export
     @RegisterProperty
     var length: Double = 320.0
 
@@ -32,7 +40,7 @@ class MoveWarningLine : Line2D() {
         texture = lineTexture
         textureMode = Line2D.LineTextureMode.TILE
         width = (lineTexture?.getHeight() ?: 1).toFloat()
-        textureRepeat = CanvasItem.TextureRepeat.DISABLED
+        textureRepeat = CanvasItem.TextureRepeat.ENABLED
         zIndex = -999
 
         points = PackedVector2Array(listOf(Vector2.ZERO, Vector2.ZERO))
@@ -44,7 +52,7 @@ class MoveWarningLine : Line2D() {
     @RegisterFunction
     override fun _process(delta: Double) {
         if (!visible) return
-        points[1] = endPoint
+        points = PackedVector2Array(listOf(Vector2.ZERO, endPoint))
     }
 
     @RegisterFunction
